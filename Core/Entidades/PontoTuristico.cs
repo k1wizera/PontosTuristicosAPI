6592386a7cc0cff8e66dc91423f4cc52ce8767d4
Otavio Faria <oaugustofaria@gmail.com>
2022-05-20 19:34:33 -0300

    Criação da entidade, contexto do banco de dados e setup do Startup

---
+++ Core/Entidades/PontoTuristico.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entidades
+{
+    public class PontoTuristico
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O Título é obrigatório.")]
+        public string Nome { get; set; }
+
+        [MaxLength(100)]
+        [Required(ErrorMessage = "A Descrição é obrigatória.")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "O endereço é obrigatório.")]
+        public string Endereco { get; set; }
+
+        [Required(ErrorMessage = "A cidade é obrigatória.")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "O estado é obrigatório.")]
+        public string Estado { get; set; }
+    }
+}
---
+++ Infraestrutura/ContextoDeArmazenamento.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infraestrutura
+{
+    public class ContextoDeArmazenamento : DbContext
+    {
+        public ContextoDeArmazenamento(DbContextOptions<ContextoDeArmazenamento> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PontoTuristico> PontosTuristicos { get; set; }
+    }
+}
